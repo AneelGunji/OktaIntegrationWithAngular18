4b63b957015e1303634a08acddc2e26fc440fa8b
Aneel <gunji.aneel@gmail.com>
2024-09-18 22:32:47 -0700

    Okta integration with angular 18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,25 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import OktaAuth, { OktaAuthOptions } from '@okta/okta-auth-js';
+import { OKTA_CONFIG, OktaAuthModule } from '@okta/okta-angular';
+
+
+const oktaConfig: OktaAuthOptions  = {
+  issuer: 'https://dev-kazhnt7br0rznvpq.us.auth0.com/oauth2/default',
+  clientId: 'njlhIfJPr6pnvnB41ZlgaacaEthm8vjw',
+  redirectUri: window.location.origin + '/login/callback',
+  scopes: ['openid', 'profile', 'email', 'offline_access'],
+  pkce: false,
+};
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    importProvidersFrom(
+      OktaAuthModule
+    ),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    { provide: OKTA_CONFIG, useValue:{oktaAuth: new OktaAuth(oktaConfig)}}]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { OktaAuthGuard, OktaCallbackComponent } from '@okta/okta-angular';
+import { ProfileComponent } from './profile/profile.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login/callback', component: OktaCallbackComponent },
+    { path: 'profile', component: ProfileComponent, canActivate: [OktaAuthGuard] },
+    { path: 'about', component: AboutComponent, canActivate: [OktaAuthGuard] },
+  ];
